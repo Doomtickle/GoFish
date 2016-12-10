eaf518f87eebe8090ff872a9397d6096a6bd0c28
Daron Adkins <daron.adkins@gmail.com>
2016-12-09 19:49:22 -0600

    cs

--- Game.h
+++ Game.h
@@ -19,10 +19,10 @@ public: // public member functions
 
             points_to_add=0; // points to add is initialized to 0
             int dicevalue = dice.rollDice(); // iDiceValue variable is assigned to rollDice function; function is called to roll dice; returns dice value
-            total = total+points.getpoints(dicevalue); // variable total is assigned to itself plus object of getpoints function;
-            // when object of getpoints function is called, the die value is assigned to the points system in the case switch
-            // getpoints function passes value of die roll; will return the total points earned in that round of game play
-            // total is is now assigned to itself to the total value returned from the getpoints function
+            total = total+ points.calculatePoints(dicevalue); // variable total is assigned to itself plus object of calculatePoints function;
+            // when object of calculatePoints function is called, the die value is assigned to the points system in the case switch
+            // calculatePoints function passes value of die roll; will return the total points earned in that round of game play
+            // total is is now assigned to itself to the total value returned from the calculatePoints function
 
 
             cout<<"You earned:"<<total<<" points in that round."<<endl; // displays isolated points earned in each round of game play
--- Points.h
+++ Points.h
@@ -11,57 +11,57 @@
 
 using namespace std; // for cout,cin
 
-class Points{ // Points class declaration
+class Points { // Points class declaration
 
 public: // public member variables
     int iDiceValue; // variable that holds value of dice; data type int
     int total; // variable that holds total points earned in game round; data type int
 
-    int getpoints(int iDiceValue){ // getPoints function to assign rolled number with points earned and item caught while fishing; passes iDiceValue
-        switch(iDiceValue){ // switch case declaration; cases 1-6 ( values of possible die rolls)
+    int calculatePoints(int iDiceValue) { // getPoints function to assign rolled number with points earned and item caught while fishing; passes iDiceValue
+        switch (iDiceValue) { // switch case declaration; cases 1-6 ( values of possible die rolls)
 
             case 1: // when 1 is rolled
-                cout<<"\nYou rolled "<<iDiceValue<<" \n"<<endl; // displays to user what number has been rolled
-                cout<<"Huge Fish \n"; // displays what the user caught while fishing
+                cout << "\nYou rolled " << iDiceValue << " \n" << endl; // displays to user what number has been rolled
+                cout << "Huge Fish \n"; // displays what the user caught while fishing
 
-                total=0+20; // total is incremented by 20
+                total = 0 + 20; // total is incremented by 20
 
 
                 break; // break
             case 2:// when 2 is rolled
-                cout<<"\nYou rolled "<<iDiceValue<<" \n"<<endl;// displays to user what number has been rolled
-                cout<<"Old shoe \n";// displays what the user caught while fishing
+                cout << "\nYou rolled " << iDiceValue << " \n" << endl;// displays to user what number has been rolled
+                cout << "Old shoe \n";// displays what the user caught while fishing
 
-                total=0+5;// total is incremented by 5
+                total = 0 + 5;// total is incremented by 5
 
                 break; // break
             case 3: // when 3 is rolled
-                cout<<"\nYou rolled "<<iDiceValue<<" \n"<<endl; // displays to user what number has been rolled
-                cout<<"Little Fish\n";// displays what the user caught while fishing
+                cout << "\nYou rolled " << iDiceValue << " \n" << endl; // displays to user what number has been rolled
+                cout << "Little Fish\n";// displays what the user caught while fishing
 
-                total=0+10; // total is incremented by 10
+                total = 0 + 10; // total is incremented by 10
 
                 break; // break
             case 4:// when the user rolls 4
-                cout<<"\nYou rolled "<<iDiceValue<<"\n"<<endl;// displays to user what number has been rolled
-                cout<<"Small prawn\n";// displays what the user caught while fishing
+                cout << "\nYou rolled " << iDiceValue << "\n" << endl;// displays to user what number has been rolled
+                cout << "Small prawn\n";// displays what the user caught while fishing
 
-                total=0+7;// total is incremented by 7
+                total = 0 + 7;// total is incremented by 7
 
                 break;// break
 
             case 5:// when the user rolls 5
-                cout<<"\nYou rolled "<<iDiceValue<<"\n"<<endl; // displays to user what number has been rolled
-                cout<<"Shark\n";// displays what the user caught while fishing
+                cout << "\nYou rolled " << iDiceValue << "\n" << endl; // displays to user what number has been rolled
+                cout << "Shark\n";// displays what the user caught while fishing
 
-                total=0+40;// total is incremented by 40
+                total = 0 + 40;// total is incremented by 40
 
                 break; // break
             case 6:// when 6 is rolled
-                cout<<"\nYou rolled "<<iDiceValue<< "\n"<<endl;// displays to user what number has been rolled
-                cout<<"Huge Catfish\n";// displays what the user caught while fishing
+                cout << "\nYou rolled " << iDiceValue << "\n" << endl;// displays to user what number has been rolled
+                cout << "Huge Catfish\n";// displays what the user caught while fishing
 
-                total=0+30;// total is incremented by 30
+                total = 0 + 30;// total is incremented by 30
 
                 break;// break
         }
